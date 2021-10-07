88a51c1754dd4e0af0e7d60f8ccf92cd06128d05
TallerThanShort <90527370+TallerThanShort@users.noreply.github.com>
2021-10-07 12:46:51 +0200

    I am so dumb
    I had message in brackets

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const client = new Discord.Client();
 
-const prefix = '?';
+const prefix = 'vw!';
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -15,21 +15,20 @@ for(const file of commandFiles){
 }
 
 client.once('ready', async () => {
-    
-    await client.user.setPresence({ activity: { name: "?help", type: "LISTENING"}, status: 'dnd'}) 
+    await client.user.setPresence({ activity: { name: "vw!help", type: "LISTENING"}, status: 'dnd'}) 
     console.log('waifu awoken')
 })
 
 
-client.on('message', (message) => {
+client.on('message', message =>{
     if (!message.guild) return;
-    if(!message.content.startsWith(prefix) || message.author.client) return;
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
     let channel = message.channel;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     
     if(command === 'help'){
-        message.lineReply(`commands: help, prefix: '?', guilds: ${client.guilds.cache.size}`);
+        message.channel.send(`commands: help, prefix: 'vw!', guilds: ${client.guilds.cache.size}`);
     } else if(command === 'ping'){
         client.commands.get('ping').execute(message, args);
     } else if(command === 'join'){
