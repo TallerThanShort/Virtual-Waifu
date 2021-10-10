bcdc3f956385a2e7765e4eca2e1297c0b65879b1
TTS <90527370+TallerThanShort@users.noreply.github.com>
2021-10-10 13:56:10 +0200

    idek
    never done this kinda garb

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     description: "ping in ms reply thing",
     execute(message, args){
-        message.channel.send('Calculating ping..').then(msg => {
+        botMessage = message.channel.send('Calculating ping..').then(msg => {
             const ping = msg.createdTimestamp - message.createdTimestamp;
         }).then(botMessage => {
             botMessage.edit(`ping is 304ms`);
