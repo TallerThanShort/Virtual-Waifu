6ccfa54e1198e1593a4eca2959fcf273dcf2c1cf
TTS <90527370+TallerThanShort@users.noreply.github.com>
2021-10-11 10:40:04 +0200

    permanent fix

--- commands/ping.js
+++ commands/ping.js
@@ -5,8 +5,6 @@ module.exports = {
         botMessage = message.channel.send('Calculating ping..').then(msg => {
             const ping = msg.createdTimestamp - message.createdTimestamp;
             msg.edit(`Ping is of ${ping}ms`);
-        }).then(botMessage => {
-            botMessage.edit(`Ping is of ${ping}ms`);
         })
     }
 }
