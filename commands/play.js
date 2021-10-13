5492de9badab0af12bb0c7cd0de8a6354b93affd
TTS <90527370+TallerThanShort@users.noreply.github.com>
2021-10-13 19:50:52 +0200

    ok better

--- commands/play.js
+++ commands/play.js
@@ -1,2 +1,7 @@
-module.exports
+module.exports = {
+    name: 'play',
+    description: "this is the command to join & play muzik",
+    execute(message, args){
+    }
+}
 // I'm dumb enough to leave this blank leave me alone!
