b01965a8f4e6a2a53598fbe43aab831f9956fc29
TTS <90527370+TallerThanShort@users.noreply.github.com>
2021-10-13 19:57:09 +0200

    bruh moment to die with

--- commands/play.js
+++ commands/play.js
@@ -2,6 +2,13 @@ module.exports = {
     name: 'play',
     description: "this is the command to join & play muzik",
     execute(message, args){
+        if(!args){
+            message.reply("Please mention a song/URL to play");
+        } else if(!message.member.voice.channel){
+            message.reply("Please connect to a voice channel!");
+        } else{
+            message.member.voice.channel.join();
+        }
     }
 }
-// I'm dumb enough to leave this blank leave me alone!
+// I'm dumb enough to leave this like this, ok??? ik it wont work
