338e6e2934654418d6c5efd52754cfdbde10f894
TTS <90527370+TallerThanShort@users.noreply.github.com>
2021-10-15 16:18:12 +0200

    Update bot to v7

--- commands/help.js
+++ commands/help.js
@@ -6,10 +6,11 @@ module.exports = {
             .setColor('RANDOM')
             .setTitle('Commands')
             .addFields(
-                {name: 'Public commands', value: 'help, ping, play, stop'},
+                {name: 'Public commands', value: 'help, ping, play, stop, waifu'},
                 {name: 'Developer-only commands', value: 'debug'},
+                {name: 'Prefix', value: 'vw!'}
             )
-            .setFooter('help for Virtual Waifu v5')
+            .setFooter('help for Virtual Waifu#5702 (895251019603259403) v7')
         
         message.channel.send(helpEmbed);
         message.react('855611027827982357');
