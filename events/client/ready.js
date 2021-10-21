e2e1140fd970d52475884ace85b11e9e2909fc42
TallerThanShort <90527370+TallerThanShort@users.noreply.github.com>
2021-10-21 10:05:16 +0200

    pls update ur status

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,5 @@
 module.exports = () =>{
+    await client.user.setPresence({ activity: { name: "vw!help", type: "LISTENING"}, status: 'dnd'}) 
     console.log('waifu awoken');
+    // pls work
 }
--- main.js
+++ main.js
@@ -11,37 +11,4 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
-// const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-// for(const file of commandFiles){
-//    const command = require(`./commands/${file}`);
-
-//    client.commands.set(command.name, command);
-// }
-
-// client.once('ready', async () => {
-//    await client.user.setPresence({ activity: { name: "vw!help", type: "LISTENING"}, status: 'dnd'}) 
-//    console.log('waifu awoken')
-//})
-
-
-//client.on('message', message =>{
-//    if (!message.guild) return;
-//    if(!message.content.startsWith(prefix) || message.author.bot) return;
-//    let channel = message.channel;
-//    const args = message.content.slice(prefix.length).split(/ +/);
-//    const command = args.shift().toLowerCase();
-//    
-//    if(command === 'help'){
-//        client.commands.get('help').execute(message, args, Discord);
-//    } else if(command === 'ping'){
-//        client.commands.get('ping').execute(message, args);
-//    } else if(command === 'play'){
-//        client.commands.get('play').execute(message, args);
-//    } else if(command === 'stop'){
-//        client.commands.get('stop').execute(message, args);
-//    } else if(command === 'waifu'){
-//        message.channel.send('https://raw.githubusercontent.com/TallerThanShort/Virtual-Waifu/main/mawaw-147v2-ghpicsay.jpg')
-//    }
-//});
-
 client.login(process.env.token);
