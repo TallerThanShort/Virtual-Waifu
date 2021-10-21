313d5cf65b5ef5127e756c1c39350811099faee5
TallerThanShort <90527370+TallerThanShort@users.noreply.github.com>
2021-10-21 10:07:54 +0200

    pls work now u async

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
-module.exports = () =>{
+module.exports = async () =>{
     await client.user.setPresence({ activity: { name: "vw!help", type: "LISTENING"}, status: 'dnd'}) 
     console.log('waifu awoken');
     // pls work
