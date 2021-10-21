3a0208662065d86414562f9b103dd6c07add1cdc
TallerThanShort <90527370+TallerThanShort@users.noreply.github.com>
2021-10-21 10:10:24 +0200

    found a quick fix

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
-module.exports = async () =>{
+module.exports = async (Discord, client) =>{
     await client.user.setPresence({ activity: { name: "vw!help", type: "LISTENING"}, status: 'dnd'}) 
     console.log('waifu awoken');
     // pls work
