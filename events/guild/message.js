f52c93b559f2b93db1f8db1cbd5bddc01b705126
TTS <90527370+TallerThanShort@users.noreply.github.com>
2021-10-23 18:50:40 +0200

    added aliases

--- events/guild/message.js
+++ events/guild/message.js
@@ -5,7 +5,7 @@ module.exports = (Discord, client, message) => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
 
-    const command = client.commands.get(cmd);
+    const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
 
     if(command) command.execute(message, args, cmd, client, Discord);
 }
