7f9f4bd90067e801bd7ace515bdd98a80bbc45d3
TTS <90527370+TallerThanShort@users.noreply.github.com>
2021-10-24 17:51:53 +0200

    updated version

--- commands/help.js
+++ commands/help.js
@@ -6,11 +6,11 @@ module.exports = {
             .setColor('RANDOM')
             .setTitle('Commands')
             .addFields(
-                {name: 'Public commands', value: 'help, ping, play, stop, waifu'},
+                {name: 'Public commands', value: 'help, ping, play, skip, stop, waifu'},
                 {name: 'Developer-only commands', value: 'debug'},
                 {name: 'Prefix', value: 'vw!'}
             )
-            .setFooter('help for Virtual Waifu#5702 (895251019603259403) v11')
+            .setFooter('help for Virtual Waifu#5702 (895251019603259403) v20')
         
         message.channel.send(helpEmbed);
         message.react('855611027827982357');
