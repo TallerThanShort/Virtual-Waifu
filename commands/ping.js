c3688c15abd97c76e4e74a61177faf826aeb1699
TTS <90527370+TallerThanShort@users.noreply.github.com>
2021-10-27 11:26:58 +0200

    fix execution policy

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'ping',
     description: "ping in ms reply thing",
-    execute(client, message, args){
+    execute(message, args, cmd, client, Discord){
         botMessage = message.channel.send('Calculating ping..').then(msg => {
             const ping = msg.createdTimestamp - message.createdTimestamp;
             msg.edit(`Ping is of ${ping}ms`);
